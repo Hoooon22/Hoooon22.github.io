1f4e3600ff89c89ee95dfdf8ca9b7175b6363433
Hoooon22 <momo990305@gmail.com>
2023-11-24 23:15:49 +0900

    korea search

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -112,7 +112,185 @@ function initSearch() {
   };
 
   request.send();
+
+  function searchLoaded(index, docs) {
+    var index = index;
+    var docs = docs;
+    var searchInput = document.getElementById('search-input');
+    var searchResults = document.getElementById('search-results');
+    var mainHeader = document.getElementById('main-header');
+    var currentInput;
+    var currentSearchIndex = 0;
+
+    function showSearch() {
+      document.documentElement.classList.add('search-active');
+    }
+
+    function hideSearch() {
+      document.documentElement.classList.remove('search-active');
+    }
+
+    function update() {
+      currentSearchIndex++;
+
+      var input = searchInput.value;
+      if (input === '') {
+        hideSearch();
+      } else {
+        showSearch();
+        // scroll search input into view, workaround for iOS Safari
+        window.scroll(0, -1);
+        setTimeout(function(){ window.scroll(0, 0); }, 0);
+      }
+      if (input === currentInput) {
+        return;
+      }
+      currentInput = input;
+      searchResults.innerHTML = '';
+      if (input === '') {
+        return;
+      }
+
+      var results = index.query(function (query) {
+        var tokens = lunr.tokenizer(input)
+        query.term(tokens, {
+          boost: 10
+        });
+        query.term(tokens, {
+          wildcard: lunr.Query.wildcard.TRAILING
+        });
+      });
+
+      if ((results.length == 0) && (input.length > 2)) {
+        var tokens = lunr.tokenizer(input).filter(function(token, i) {
+          return token.str.length < 20;
+        })
+        if (tokens.length > 0) {
+          results = index.query(function (query) {
+            query.term(tokens, {
+              editDistance: Math.round(Math.sqrt(input.length / 2 - 1))
+            });
+          });
+        }
+      }
+
+      if (results.length == 0) {
+        var noResultsDiv = document.createElement('div');
+        noResultsDiv.classList.add('search-no-result');
+        noResultsDiv.innerText = 'No results found';
+        searchResults.appendChild(noResultsDiv);
+
+      } else {
+        var resultsList = document.createElement('ul');
+        resultsList.classList.add('search-results-list');
+        searchResults.appendChild(resultsList);
+
+        addResults(resultsList, results, 0, 10, 100, currentSearchIndex);
+      }
+
+      function addResults(resultsList, results, start, batchSize, batchMillis, searchIndex) {
+        if (searchIndex != currentSearchIndex) {
+          return;
+        }
+        for (var i = start; i < (start + batchSize); i++) {
+          if (i == results.length) {
+            return;
+          }
+          addResult(resultsList, results[i]);
+        }
+        setTimeout(function() {
+          addResults(resultsList, results, start + batchSize, batchSize, batchMillis, searchIndex);
+        }, batchMillis);
+      }
+
+      function addResult(resultsList, result) {
+        var doc = docs[result.ref];
+
+        var resultsListItem = document.createElement('li');
+        resultsListItem.classList.add('search-results-list-item');
+        resultsList.appendChild(resultsListItem);
+
+        var resultLink = document.createElement('a');
+        resultLink.classList.add('search-result');
+        resultLink.setAttribute('href', doc.url);
+        resultsListItem.appendChild(resultLink);
+
+        var resultTitle = document.createElement('div');
+        resultTitle.classList.add('search-result-title');
+        resultLink.appendChild(resultTitle);
+
+        // note: the SVG svg-doc is only loaded as a Jekyll include if site.search_enabled is true; see _includes/icons/icons.html
+        var resultDoc = document.createElement('div');
+        resultDoc.classList.add('search-result-doc');
+        resultDoc.innerHTML = '<svg viewBox="0 0 24 24" class="search-result-icon"><use xlink:href="#svg-doc"></use></svg>';
+        resultTitle.appendChild(resultDoc);
+
+        var resultDocTitle = document.createElement('div');
+        resultDocTitle.classList.add('search-result-doc-title');
+        resultDocTitle.innerHTML = doc.doc;
+        resultDoc.appendChild(resultDocTitle);
+        var resultDocOrSection = resultDocTitle;
+
+        if (doc.doc != doc.title) {
+          resultDoc.classList.add('search-result-doc-parent');
+          var resultSection = document.createElement('div');
+          resultSection.classList.add('search-result-section');
+          resultSection.innerHTML = doc.title;
+          resultTitle.appendChild(resultSection);
+          resultDocOrSection = resultSection;
+        }
+
+        var metadata = result.matchData.metadata;
+        var titlePositions = [];
+        var contentPositions = [];
+        for (var j in metadata) {
+          var meta = metadata[j];
+          if (meta.title) {
+            var positions = meta.title.position;
+            for (var k in positions) {
+              titlePositions.push(positions[k]);
+            }
+          }
+          if (meta.content) {
+            var positions = meta.content.position;
+            for (var k in positions) {
+              var position = positions[k];
+              var previewStart = position[0];
+              var previewEnd = position[0] + position[1];
+              var ellipsesBefore = true;
+              var ellipsesAfter = true;
+              for (var k = 0; k < {{ site.search.preview_words_before | default: 5 }}; k++) {
+                var nextSpace = doc.content.lastIndexOf(' ', previewStart - 2);
+                var nextDot = doc.content.lastIndexOf('. ', previewStart - 2);
+                if ((nextDot >= 0) && (nextDot > nextSpace)) {
+                  previewStart = nextDot + 1;
+                  ellipsesBefore = false;
+                  break;
+                }
+                if (nextSpace < 0) {
+                  previewStart = 0;
+                  ellipsesBefore = false;
+                  break;
+                }
+                previewStart = nextSpace + 1;
+              }
+              // ...
+            }
+          }
+        }
+
+        // ...
+      }
+
+      // ...
+    }
+
+    // ...
+  }
+
+  // ...
 }
+
 
 function searchLoaded(index, docs) {
   var index = index;
