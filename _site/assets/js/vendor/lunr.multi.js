58874ccdda2c056c95440005a949a43d9f825468
Hoooon22 <momo990305@gmail.com>
2023-11-24 22:35:52 +0900

    korea search

--- _site/assets/js/vendor/lunr.multi.js
+++ _site/assets/js/vendor/lunr.multi.js
@@ -34,7 +34,7 @@
            language. This could mean that sometimes words that have the same
            stemming root will not be stemmed as such.
            */
-        lunr.multiLanguage = function(/* lang1, lang2, ... */) {
+        lunr.multiLanguage = function(en, ko) {
             var languages = Array.prototype.slice.call(arguments);
             var nameSuffix = languages.join('-');
             var wordCharacters = "";
